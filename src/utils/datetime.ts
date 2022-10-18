7cc2c1342c1ecb1ce723295a4e1fbe5de1ce04b8
abcworld123 <rkaxhdals@gmail.com>
2022-10-18 19:49:36 +0900

    Add datetime utils

--- src/utils/datetime.ts
+++ src/utils/datetime.ts
@@ -1,24 +1,33 @@
-import moment from 'moment-timezone';
-
 function hourMinToSec(hhmm: string) {
-  const hourSec = parseInt(hhmm.substring(0, 2)) * 3600000;
-  const minSec = parseInt(hhmm.substring(2, 4)) * 60000;
+  const hourSec = parseInt(hhmm.slice(0, 2)) * 3600000;
+  const minSec = parseInt(hhmm.slice(2, 4)) * 60000;
   return hourSec + minSec;
 }
 
 export function getDateTime(_offset: string, _interval: string, _delay: string): [date: string, time: string] {
-  let ms = +moment.tz('Asia/Seoul');
-  const utcOffset = moment.tz.zone('Asia/Seoul').utcOffset(ms) * 60000;
+  let ms = Date.now();
+  const utcOffset = 32400000;
   const offset = hourMinToSec(_offset);
   const interval = hourMinToSec(_interval);
   const delay = hourMinToSec(_delay);
-  ms -= utcOffset;
+  ms += utcOffset;
   ms -= offset;
   ms -= delay;
   ms -= ms % interval;
   ms += offset;
-  ms += utcOffset;
-  const date = moment(ms).format('YYYYMMDD');
-  const time = moment(ms).format('HHmm');
+  // ms -= utcOffset;
+  const datetime = new Date(ms).toISOString().replace(/[T\-:]|\..+/g, '').slice(0, 12);
+  const date = datetime.slice(0, 8);
+  const time = datetime.slice(8, 12);
   return [date, time];
+}
+
+export function addDay(_date: string, amount: number) {
+  const dayMs = 86400000;
+  const _year = _date.slice(0, 4);
+  const _month = _date.slice(4, 6);
+  const _day = _date.slice(6, 8);
+  _date = `${_year}-${_month}-${_day}`;
+  const date = new Date(new Date(_date).getTime() + (dayMs * amount)).toISOString().replace('-', '').slice(0, 8);
+  return date;
 }
