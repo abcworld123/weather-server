6214c08d9fd57b3f209da5f000ac22a1958ff2ce
abcworld123 <rkaxhdals@gmail.com>
2022-10-22 15:42:33 +0900

    Add schedulejob logs

--- src/schedules/index.ts
+++ src/schedules/index.ts
@@ -12,12 +12,49 @@ export default function runScheduleJobs() {
   ruleDayLong.hour = [2, 5, 8, 11, 14, 17, 20, 23];
   ruleWeek.hour = [6, 18];
 
-  scheduleJob(ruleDayShortNow, updateDayShortNow);
-  scheduleJob(ruleDayLong, updateDayLong);
-  scheduleJob(ruleWeek, updateWeek);
+  scheduleJob(ruleDayShortNow, runDayShortNow);
+  scheduleJob(ruleDayLong, runDayLong);
+  scheduleJob(ruleWeek, runWeek);
 }
 
-async function updateDayShortNow() {
-  await updateDayShort();
-  await updateDayNow();
+async function runDayShortNow() {
+  try {
+    await updateDayShort();
+    await updateDayNow();
+    logInfo('runDayShortNow', 'success');
+  } catch (err) {
+    logError('runDayShortNow', err.message);
+
+  }
+}
+
+async function runDayLong() {
+  try {
+    await updateDayLong();
+    logInfo('runDayLong', 'success');
+  } catch (err) {
+    logError('runDayLong', err.message);
+
+  }
+}
+
+async function runWeek() {
+  try {
+    await updateWeek();
+    logInfo('runWeek', 'success');
+  } catch (err) {
+    logError('runWeek', err.message);
+
+  }
+}
+
+function logInfo(title: string, msg: string) {
+  const dt = new Date().toISOString();
+  console.info(`\x1B[32m[INFO] [${dt}] ${title}: ${msg}\x1B[0m`);
+}
+
+function logError(title: string, msg: string) {
+  const dt = new Date().toISOString();
+  console.error(`\x1B[31m[ERROR] [${dt}] ${title}: ${msg}\x1B[0m`);
+
 }
